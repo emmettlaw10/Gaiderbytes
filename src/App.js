339228f16af25f3968eb1390ebdf70fac27117df
Emmett Law <emmettlaw@theurbanwriters.com>
2023-04-11 00:40:18 -0400

    rebuilt

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
 
   const handleSave = async (values) => {
     try {
-      const response = await fetch("/studentApplication", {
+      const response = await fetch("http://localhost:5000/studentApplication", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -79,7 +79,7 @@ function App() {
 
   const handleSaveCoach = async (formData) => {
     try {
-      const response = await fetch("/coachApplication", {
+      const response = await fetch("http://localhost:5000/coachApplication", {
         method: "POST",
         body: formData,
       });
